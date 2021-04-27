5f90376bb921c6aa3fce58f21c1da42c02613cc2
torchu <victor.cabritag@gmail.com>
2021-04-27 21:38:05 +0200

    docker conf and routes

---
+++ app/src/app.py
@@ -0,0 +1,93 @@
+from flask import Flask, request, Response
+from pymongo import MongoClient
+from bson import json_util
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+client = MongoClient("mongo", 27017)
+db = client.SampleCollections
+
+
+@app.route('/hello-world', methods=['GET'])
+def hello_world():
+  return {'message': 'hello world'};
+
+
+@app.route('/anime', methods=['GET'])
+def list_anime():
+  anime_list = db.anime.find()
+  response = json_util.dumps(anime_list)
+  return Response(response, mimetype='application/json')
+
+
+@app.route('/anime/<id>', methods=['GET'])
+def get_anime(id):
+  anime = db.anime.find_one({'_id': ObjectId(id)})
+  response = json_util.dumps(anime)
+  return Response(response, mimetype='application/json')
+
+
+@app.route('/anime', methods=['POST'])
+def create_anime():
+  title = request.json['title']
+  img = request.json['img']
+  n_episodes = request.json['n_episodes']
+  status = request.json['status']
+
+  if title and img and n_episodes and status:
+    id = db.anime.insert_one(
+      {
+        'title': title,
+        'img': img,
+        'n_episodes': n_episodes,
+        'status': status
+      }
+    )
+    response = {
+      'id': str(id),
+      'title': title,
+      'img': img,
+      'n_episodes': n_episodes,
+      'status': status
+    }
+
+    return response
+
+  else:
+    return {'error': 'Invalid data'}
+
+@app.route('/anime/<id>', methods=['PUT'])
+def edit_anime(id):
+  title = request.json['title']
+  img = request.json['img']
+  n_episodes = request.json['n_episodes']
+  status = request.json['status']
+
+  if title and img and n_episodes and status:
+    db.anime.update_one({'_id': ObjectId(id)},
+      {
+        '$set': {
+          'title': title,
+          'img': img,
+          'n_episodes': n_episodes,
+          'status': status
+        }
+      }
+    )
+    response = {
+      'id': id,
+      'title': title,
+      'img': img,
+      'n_episodes': n_episodes,
+      'status': status
+    }
+
+    return response
+
+
+@app.route('/anime/<id>', methods=['DELETE'])
+def delete_anime(id):
+  db.anime.delete_one({'_id': ObjectId(id)})
+  return {'message': 'Deleted succesfully'};
+
--- src/services/aaa.py
+++
@@ -1,7 +0,0 @@
-from src.app import app
-
-route = '/aaa'
-
-@app.route(route + '/hello')
-def hello_world2():
-  return {'message': 'hello world from service'};
